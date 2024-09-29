a5e3c0a4bdbbc8e58cc016951293e7c848a6bef9
pradeepmalik2001 <malikpradeep02022001@gmail.com>
2024-09-29 21:19:02 +0530

    first commit

---
+++ src/Pages/Header.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import '../Pages/Header.css';
+import logo from '../Images/logo.png';
+import midlogo from '../Images/Webimage.jpg';
+
+function Header() {
+  return (
+    <div>
+      <header>
+      <nav className="navbar">
+        <div className="navbar-left">
+          <ul className="nav-links">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#team">Team</a></li>
+            <li><a href="#what-we-offer">What We Offer</a></li>
+          </ul>
+        </div>
+
+        <div className="navbar-center">
+          <a href="#logo">
+            <img
+              className="logo"
+              src={logo}
+              alt="Logo"
+            />
+          </a>
+        </div>
+
+        <div className="navbar-right">
+          <ul className="nav-links">
+            <li><a href="#podcasts">Podcasts</a></li>
+            <li><a href="#blog">Blog</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="header-banner">
+        <p className="banner-text">
+          Speak To A Team Member To Answer Your Questions Now!
+        </p>
+        <a href="#schedule" className="schedule-button">
+          Schedule a free call now!
+        </a>
+      </div>
+    </header>
+
+    <div className="mid-logo">
+          <a href="#mid-logo">
+            <img
+              className="mid-logo"
+              src={midlogo}
+              alt="Logo"
+            />
+          </a>
+    </div>
+
+    </div>
+  )
+}
+
+export default Header
