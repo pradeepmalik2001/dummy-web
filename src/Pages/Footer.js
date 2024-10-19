10f829c519e068fdce1caa3b845ebbfc06ea913e
saurabhSingh <saurabhsingh3198@gmail.com>
2024-10-19 20:35:28 +0530

    code marge

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -23,19 +23,19 @@ const Footer = () => {
       </div> */}
       
       <div className="footer-social">
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="social-icon">
           <i className="fab fa-instagram"></i>
         </a>
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="social-icon">
           <i className="fab fa-facebook-f"></i>
         </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="social-icon">
           <i className="fab fa-linkedin-in"></i>
         </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="social-icon">
           <i className="fab fa-twitter"></i>
         </a>
-        <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://youtube.com" target="_blank" rel="noopener noreferrer" className="social-icon">
           <i className="fab fa-youtube"></i>
         </a>
       </div>
