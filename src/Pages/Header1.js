5724832908af714cfe75a79a4ef3214f7a8dd5dc
Pradeep Malik <pradeep.malik@fusionfin.com>
2025-10-06 21:44:19 +0530

    Fix header

--- src/Pages/Header1.js
+++ src/Pages/Header1.js
@@ -1,41 +1,43 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../Images/logo.png';
 import { Link } from 'react-router-dom';
 import './Header1.css';
 
 const Header1 = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <div>
-   <nav className="navbar">
-        <div className="navbar-left">
-          <ul className="nav-links">
-            {/* <li><a href="#home">Home</a></li> */}
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/">Family</Link></li>
-            <li><Link>What We Offer</Link></li>
-          </ul>
-        </div>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <div className="navbar-brand">
+            <Link to="/">
+              <img className="logo" src={logo} alt="Logo" />
+            </Link>
+          </div>
+          
+          <button className="mobile-menu-btn" onClick={toggleMenu}>
+            <span className={`hamburger ${isMenuOpen ? 'active' : ''}`}></span>
+            <span className={`hamburger ${isMenuOpen ? 'active' : ''}`}></span>
+            <span className={`hamburger ${isMenuOpen ? 'active' : ''}`}></span>
+          </button>
 
-        <div className="navbar-center">
-          <a href="#logo">
-            <img
-              className="logo"
-              src={logo}
-              alt="Logo"
-            />
-          </a>
-        </div>
-
-        <div className="navbar-right">
-          <ul className="nav-links">
-            {/* <li><a href='#podcasts'>Podcasts</a></li> */}
-            <li><Link to="/podcasts">Podcasts</Link></li>
-            <li><Link to="/blog">Blog</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/signIn" className="signin-btn">Sign In</Link></li>
-          </ul>
+          <div className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
+            <ul className="nav-links">
+              <li><Link to="/" onClick={() => setIsMenuOpen(false)}>Home</Link></li>
+              <li><Link to="/" onClick={() => setIsMenuOpen(false)}>Family</Link></li>
+              <li><Link to="/" onClick={() => setIsMenuOpen(false)}>What We Offer</Link></li>
+              <li><Link to="/podcasts" onClick={() => setIsMenuOpen(false)}>Podcasts</Link></li>
+              <li><Link to="/blog" onClick={() => setIsMenuOpen(false)}>Blog</Link></li>
+              <li><Link to="/contact" onClick={() => setIsMenuOpen(false)}>Contact</Link></li>
+              <li><Link to="/signIn" className="signin-btn" onClick={() => setIsMenuOpen(false)}>Sign In</Link></li>
+            </ul>
+          </div>
         </div>
       </nav>
     </div>
